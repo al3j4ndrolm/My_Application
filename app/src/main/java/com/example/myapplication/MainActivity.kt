53f21b0474a88a7a96efbc45270766b5c4ea0bc9
al3j4ndrolm <lopezalejandro5b@gmail.com>
2023-09-15 00:11:58 -0700

    CuteCat_5 - Change the background, Cat photos to real cats, add petting action, interactive photos, redesign top banners

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,14 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -24,6 +26,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -57,12 +60,27 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     var result by remember { mutableStateOf(1) }
     val imageResource = when (result) {
-        1 -> R.drawable.funny_cat_1
-        2 -> R.drawable.funny_cat_2
+        1 -> R.drawable.tails
+        2 -> R.drawable.bruno
         else -> {
-            R.drawable.funnny_cat_3
+            R.drawable.tabby
         }
     }
+    val catName = when (result) {
+        1 -> "Tails"
+        2 -> "Bruno"
+        else -> {
+            "Tabby"
+        }
+    }
+    var isCatHappy by remember {
+        mutableStateOf(false)
+    }
+
+    val actionText =
+        if (isCatHappy) "$catName is happier after your petting!"
+        else "$catName seems to want pet..."
+
     Box(modifier = Modifier) {
         Column(
             verticalArrangement = Arrangement.Center
@@ -70,19 +88,19 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         {
             Box(
                 modifier = Modifier
-                    .background(color = Color.hsl(199f, 0.70f, 0.87f))
+                    .background(color = Color.hsl(202f, 0.65f, 0.85f))
                     .size(400.dp, 55.dp)
             ) {
                 Row(
                     modifier = Modifier
-                        .size(200.dp, 55.dp)
+                        .size(400.dp, 55.dp)
                         .align(Alignment.Center),
                     horizontalArrangement = Arrangement.Center
                 ) {
                     Text(
                         modifier = Modifier
-                            .size(150.dp, 55.dp),
-                        text = "CuteCat",
+                            .size(200.dp, 55.dp),
+                        text = "Los Gatos",
                         fontSize = 40.sp,
                         lineHeight = 0.sp,
                         textAlign = TextAlign.Center,
@@ -98,41 +116,22 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                 }
 
             }
-            Box(
-                modifier = Modifier.align(Alignment.End)
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.funny_cat_blog_15_desktop_background),
-                    contentDescription = "Welcome_Photo"
-                )
-                Row(
-                    modifier = Modifier
-                        .align(Alignment.BottomStart)
-                ) {
-                    Text(
-                        text = "TODAY CAT:",
-                        fontSize = 35.sp,
-                        fontWeight = FontWeight(1000),
-                        fontStyle = FontStyle.Normal,
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier
-                            .size(210.dp, 50.dp)
-                    )
-                    Text(
-                        text = "Butters",
-                        fontSize = 35.sp,
-                        fontWeight = FontWeight(250),
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier
-                            .size(150.dp, 50.dp)
-                    )
-                }
-            }
+            Text(
+                text = "Hello $catName !",
+                fontSize = 35.sp,
+                fontWeight = FontWeight(350),
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(color = Color.hsl(202f, 0.65f, 0.85f))
+                    .padding(20.dp)
+            )
+
             Box(
                 modifier = Modifier.fillMaxWidth()
             ) {
                 Image(
-                    painter = painterResource(id = R.drawable.background_paws),
+                    painter = painterResource(id = R.drawable.background),
                     contentDescription = "Background",
                     contentScale = ContentScale.Crop,
                     modifier = Modifier
@@ -144,28 +143,54 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                         .fillMaxWidth(),
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Image(
-                        modifier = Modifier
-                            .size(300.dp, 300.dp),
-                        contentScale = ContentScale.Crop,
-                        painter = painterResource(imageResource),
-                        contentDescription = "Funny_cat_1"
-                    )
+                    Box(modifier = Modifier.clickable { isCatHappy = !isCatHappy }) {
+                        Image(
+                            modifier = Modifier
+                                .size(300.dp, 300.dp)
+                                .clip(RoundedCornerShape(24.dp)),
+                            contentScale = ContentScale.Crop,
+                            painter = painterResource(imageResource),
+                            contentDescription = "Funny_cat_1"
+                        )
+                    }
+
                     Row(
-                        modifier = Modifier.fillMaxWidth(),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(20.dp),
                         horizontalArrangement = Arrangement.SpaceAround
                     ) {
-                        Button(onClick = {
-                            result = result - 1
-                        }, enabled = result > 1) {
-                            Text(stringResource(R.string.last_button))
+                        Button(
+                            onClick = {
+                                result = result - 1
+                                isCatHappy = false
+                            },
+                            enabled = result > 1,
+                        ) {
+
+                            Text(
+                                stringResource(R.string.last_button),
+                                fontSize = 20.sp
+                            )
                         }
                         Button(onClick = {
                             result = result + 1
+                            isCatHappy = false
                         }, enabled = result < 3) {
-                            Text(stringResource(R.string.next_button))
+                            Text(
+                                stringResource(R.string.next_button),
+                                fontSize = 20.sp
+                            )
                         }
                     }
+                    Text(
+                        text = actionText,
+                        fontSize = 25.sp,
+                        textAlign = TextAlign.Center,
+                        color = Color.hsl(200f, 0.70f, 0.25f),
+                        modifier = Modifier
+                            .padding(horizontal = 10.dp, vertical = 20.dp)
+                    )
                 }
             }
         }
